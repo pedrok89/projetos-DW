695dffd0903ab6bb3c6b2d37917a533171865c7c
Pedro Ivo Da Silva <35647447+pedrok89@users.noreply.github.com>
2022-09-24 17:08:16 -0300

    projetos cores
    update

---
+++ app.js
@@ -0,0 +1,38 @@
+
+	function cor(){
+
+	let aleatoriedade = Math.floor(Math.random() * 3)
+
+	if (aleatoriedade == 0) {
+
+		return 'cor1'
+
+	} else if (aleatoriedade == 1) {
+
+		return 'cor2'
+
+	} else if (aleatoriedade == 2) {
+
+		return 'cor3'
+
+	}
+
+	}
+
+	setInterval(function (){
+
+		let elemento = document.getElementById('mudanca')
+
+		let elemento2 = document.getElementById('project')
+
+			cor()
+
+		elemento.className = "row d-flex justify-content-center" + ' ' + cor()
+
+		elemento2.className = cor()
+
+		}, 7000)
+
+	
+
+	
