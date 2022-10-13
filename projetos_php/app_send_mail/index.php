a2603b8751962001522a646d0c1824a1bb8a2cbd
Pedro Ivo Da Silva <35647447+pedrok89@users.noreply.github.com>
2022-10-13 14:59:40 -0300

    projetos no webhost
    update

--- projetos_php/app_send_mail/index.php
+++ projetos_php/app_send_mail/index.php
@@ -5,9 +5,9 @@
 
     	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    	<link rel="stylesheet" type="text/css" href="css.css">
+    	<link rel="stylesheet" type="text/css" href="../../css.css">
 
-    	<link rel="shortcut icon" type="image/png" href="meu_logo.png">
+    	<link rel="shortcut icon" type="image/png" href="../../imagens/meu_logo.png">
 
     	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5968126729205451"
      crossorigin="anonymous"></script>
